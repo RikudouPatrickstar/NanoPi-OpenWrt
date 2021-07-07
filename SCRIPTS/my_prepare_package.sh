3641c1181b959e235d8e850faefb64c4931ccce1
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2021-07-07 11:16:49 +0800

    替换 OpenClash 控制面板为最新版本

--- SCRIPTS/my_prepare_package.sh
+++ SCRIPTS/my_prepare_package.sh
@@ -92,6 +92,24 @@ rm -fr package/new/OpenClash
 git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/new/OpenClash
 mv package/new/OpenClash/luci-app-openclash package/new/luci-app-openclash
 rm -fr package/new/OpenClash
+## 编译并使用最新的控制面板
+git clone -b master --single-branch https://github.com/haishanh/yacd.git
+pushd yacd
+  yarn
+  yarn build
+popd
+git clone -b master --single-branch https://github.com/Dreamacro/clash-dashboard.git
+pushd clash-dashboard
+  yarn
+  yarn build
+popd
+pushd package/new/luci-app-openclash/root/usr/share/openclash
+  rm -fr dashboard yacd
+  mv ${OP_SC_DIR}/yacd/public ./yacd
+  mv ${OP_SC_DIR}/clash-dashboard/dist ./dashboard
+popd
+rm -fr ${OP_SC_DIR}/yacd
+rm -fr ${OP_SC_DIR}/clash-dashboard
 ## 预置 Clash 内核
 clash_dev_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
 clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
