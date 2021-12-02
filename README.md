c0f3e5010472b9f02c76d26360206e497f203a45
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2021-12-02 12:25:51 +0800

    移除 OpenClash

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
 
 4. 插件清单：
 
-    - 应用：[`OpenClash`](https://github.com/vernesong/OpenClash/tree/master) [`SSRPlus`](https://github.com/fw876/helloworld/tree/master)
+    - 应用：[`SSRPlus`](https://github.com/fw876/helloworld/tree/master)
     
     - 主题：[`Argon（默认）`](https://github.com/jerrykuku/luci-theme-argon/tree/master) [`Bootstrap`](https://github.com/openwrt/luci/tree/openwrt-21.02/themes/luci-theme-bootstrap)
 
--- SCRIPTS/my_prepare_package.sh
+++ SCRIPTS/my_prepare_package.sh
@@ -83,41 +83,41 @@ pushd package/lean/luci-app-ssr-plus
   done
 popd
 
-# OpenClash
-rm -fr package/new/luci-app-openclash
-rm -fr package/new/OpenClash
-git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/new/OpenClash
-mv package/new/OpenClash/luci-app-openclash package/new/luci-app-openclash
-rm -fr package/new/OpenClash
-## 编译并使用最新的控制面板
-git clone -b master --single-branch https://github.com/haishanh/yacd.git
-pushd yacd
-  yarn
-  yarn build
-popd
-git clone -b master --single-branch https://github.com/Dreamacro/clash-dashboard.git
-pushd clash-dashboard
-  yarn
-  yarn build
-popd
-pushd package/new/luci-app-openclash/root/usr/share/openclash
-  rm -fr dashboard yacd
-  mv ${OP_SC_DIR}/yacd/public ./yacd
-  mv ${OP_SC_DIR}/clash-dashboard/dist ./dashboard
-popd
-rm -fr ${OP_SC_DIR}/yacd
-rm -fr ${OP_SC_DIR}/clash-dashboard
-## 预置 Clash 内核
-clash_dev_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-clash_premium_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
-mkdir -p package/base-files/files/etc/openclash/core
-pushd package/base-files/files/etc/openclash/core
-  wget -qO- $clash_dev_url | tar xOvz > clash
-  wget -qO- $clash_game_url | tar xOvz > clash_game
-  wget -qO- $clash_premium_url | gunzip -c > clash_tun
-  chmod +x clash*
-popd
+# # OpenClash
+# rm -fr package/new/luci-app-openclash
+# rm -fr package/new/OpenClash
+# git clone -b master --depth 1 https://github.com/vernesong/OpenClash package/new/OpenClash
+# mv package/new/OpenClash/luci-app-openclash package/new/luci-app-openclash
+# rm -fr package/new/OpenClash
+# ## 编译并使用最新的控制面板
+# git clone -b master --single-branch https://github.com/haishanh/yacd.git
+# pushd yacd
+#   yarn
+#   yarn build
+# popd
+# git clone -b master --single-branch https://github.com/Dreamacro/clash-dashboard.git
+# pushd clash-dashboard
+#   yarn
+#   yarn build
+# popd
+# pushd package/new/luci-app-openclash/root/usr/share/openclash
+#   rm -fr dashboard yacd
+#   mv ${OP_SC_DIR}/yacd/public ./yacd
+#   mv ${OP_SC_DIR}/clash-dashboard/dist ./dashboard
+# popd
+# rm -fr ${OP_SC_DIR}/yacd
+# rm -fr ${OP_SC_DIR}/clash-dashboard
+# ## 预置 Clash 内核
+# clash_dev_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+# clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+# clash_premium_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+# mkdir -p package/base-files/files/etc/openclash/core
+# pushd package/base-files/files/etc/openclash/core
+#   wget -qO- $clash_dev_url | tar xOvz > clash
+#   wget -qO- $clash_game_url | tar xOvz > clash_game
+#   wget -qO- $clash_premium_url | gunzip -c > clash_tun
+#   chmod +x clash*
+# popd
 
 # 调整默认 LAN IP
 sed -i 's/192.168.1.1/192.168.24.1/g' package/base-files/files/bin/config_generate
