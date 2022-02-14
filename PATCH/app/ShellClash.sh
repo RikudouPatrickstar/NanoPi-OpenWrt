cdac940149a0c0d97a89d5d2dcbf0db947362416
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2022-02-14 15:30:06 +0800

    Update URL

--- PATCH/app/ShellClash.sh
+++ PATCH/app/ShellClash.sh
@@ -6,25 +6,25 @@ pushd package/base-files/files
   mkdir -p etc/clash
   mkdir -p etc/clash/ui
   mkdir -p etc/init.d
-  shellclash_version=$(curl -sL https://github.com/juewuy/ShellClash/raw/master/bin/version | grep "versionsh" | awk -F "=" '{print $2}')
+  shellclash_version=$(curl -sL https://raw.githubusercontent.com/juewuy/ShellClash/master/bin/version | grep "versionsh" | awk -F "=" '{print $2}')
   pushd etc/clash
     ## 脚本及 Clash Premium Core
-    wget https://github.com/juewuy/ShellClash/raw/master/bin/clashfm.tar.gz -O - | tar xz -C ./
-    wget https://github.com/juewuy/ShellClash/raw/master/bin/clashpre/clash-linux-armv8 -O clash
+    wget https://raw.githubusercontent.com/juewuy/ShellClash/master/bin/clashfm.tar.gz -O - | tar xz -C ./
+    wget https://raw.githubusercontent.com/juewuy/ShellClash/master/bin/clashpre/clash-linux-armv8 -O clash
     chmod 777 *
     ## 启动文件
     mv clashservice ../init.d/clash
     ## 地址库
-    wget https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb -O Country.mmdb
+    wget https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/Country.mmdb -O Country.mmdb
     ## 控制面板
-    wget https://github.com/juewuy/ShellClash/raw/master/bin/clashdb.tar.gz -O - | tar xz -C ./ui
+    wget https://raw.githubusercontent.com/juewuy/ShellClash/master/bin/clashdb.tar.gz -O - | tar xz -C ./ui
     sed -i "s/127.0.0.1/192.168.24.1/g" ui/assets/*.js
     sed -i "s/9090/9999/g" ui/assets/*.js
     ## 创建相关文件
     touch log mac mark
     ## 配置标记文件
     echo "versionsh_l=${shellclash_version}" >> mark
-    echo "update_url=http://shellclash.ga/" >> mark
+    echo "update_url=https://raw.githubusercontents.com/juewuy/ShellClash/master" >> mark
     echo "userguide=1" >> mark
     echo "redir_mod=混合模式" >> mark
     echo "clashcore=clashpre" >> mark
--- PATCH/app/v2rayA.sh
+++ PATCH/app/v2rayA.sh
@@ -18,9 +18,9 @@ pushd package/base-files/files
   v2raya_version=$(curl -sL https://api.github.com/repos/v2rayA/v2rayA/releases/latest | grep tag_name | sed 's/.*v//;s/".*//')
   wget https://github.com/v2rayA/v2rayA/releases/download/v$v2raya_version/v2raya_linux_arm64_$v2raya_version -O usr/bin/v2raya
   chmod +x usr/bin/v2raya
-  wget https://github.com/openwrt/packages/raw/master/net/v2raya/files/v2raya.config -O etc/config/v2raya
+  wget https://raw.githubusercontent.com/openwrt/packages/master/net/v2raya/files/v2raya.config -O etc/config/v2raya
   sed -i "s/option enabled '0'/option enabled '1'/" etc/config/v2raya
-  wget https://github.com/openwrt/packages/raw/master/net/v2raya/files/v2raya.init -O etc/init.d/v2raya
+  wget https://raw.githubusercontent.com/openwrt/packages/master/net/v2raya/files/v2raya.init -O etc/init.d/v2raya
   chmod +x etc/init.d/v2raya
 popd
 
