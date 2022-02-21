7e13f5da21fdb775a05b325e8fce657237cb4870
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2022-02-21 14:35:43 +0800

    去除多余信息显示

--- SCRIPTS/my_prepare_package.sh
+++ SCRIPTS/my_prepare_package.sh
@@ -26,6 +26,9 @@ sed -i '/8152/d' 02_R2S.sh
 sed -i 's/+luci-app-opkg //' ./feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' ./feeds/luci/collections/luci/Makefile
 
+# 去除多余信息显示
+sed -i '/Target Platform/d' ./feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/10_system.js
+
 # Argon 主题
 bash ${OP_SC_DIR}/../PATCH/app/Argon.sh
 
