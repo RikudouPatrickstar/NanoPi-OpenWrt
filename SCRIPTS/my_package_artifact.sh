4251eff3b1c25009951d1bd4dabfa186d3e50641
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2023-03-23 19:33:24 +0800

    调整分区大小

--- SCRIPTS/my_package_artifact.sh
+++ SCRIPTS/my_package_artifact.sh
@@ -4,7 +4,7 @@ mv ./.config ./bin/targets/rockchip/armv8/config-full.buildinfo
 pushd ./bin/targets/rockchip/armv8/
     gzip -d *.gz
     mkdir ./ext4_firmware
-    mount -o loop,offset=67108864 *ext4*.img ./ext4_firmware
+    mount -o loop,offset=100663296 *ext4*.img ./ext4_firmware
     pushd ./ext4_firmware
         rm -fr overlay rom sbin/firstboot
     popd
