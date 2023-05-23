3bf90dceb3825a931b63a56be59a675adb877833
RikudouPatrickstar <68163551+RikudouPatrickstar@users.noreply.github.com>
2023-05-23 16:34:37 +0800

    Argon移除底部文字跟进上游变化

--- PATCH/app/Argon.sh
+++ PATCH/app/Argon.sh
@@ -9,12 +9,12 @@ git clone -b master --single-branch https://github.com/jerrykuku/luci-theme-argo
 pushd package/new/luci-theme-argon
   pushd luasrc/view/themes/argon
     ## 移除 footer.htm 底部文字
-    sed -i '/<a class=\"luci-link\" href=\"https:\/\/github.com\/openwrt\/luci\">/d' footer.htm
-    sed -i '/<a href=\"https:\/\/github.com\/jerrykuku\/luci-theme-argon\">/d' footer.htm
+    sed -i '/<a class=\"luci-link\" href=\"https:\/\/github.com\/openwrt\/luci\"/d' footer.htm
+    sed -i '/<a href=\"https:\/\/github.com\/jerrykuku\/luci-theme-argon\"/d' footer.htm
     sed -i '/<%= ver.distversion %>/d' footer.htm
     ## 移除 footer_login.htm 底部文字
-    sed -i '/<a class=\"luci-link\" href=\"https:\/\/github.com\/openwrt\/luci\">/d' footer_login.htm
-    sed -i '/<a href=\"https:\/\/github.com\/jerrykuku\/luci-theme-argon\">/d' footer_login.htm
+    sed -i '/<a class=\"luci-link\" href=\"https:\/\/github.com\/openwrt\/luci\"/d' footer_login.htm
+    sed -i '/<a href=\"https:\/\/github.com\/jerrykuku\/luci-theme-argon\"/d' footer_login.htm
     sed -i '/<%= ver.distversion %>/d' footer_login.htm
   popd
   pushd htdocs/luci-static/argon
